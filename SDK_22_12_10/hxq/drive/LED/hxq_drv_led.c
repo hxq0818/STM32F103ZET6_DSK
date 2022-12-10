c65a3c38bc7b3abd7f1cdf47fbeb7cfcb820f8ff
thinkbook16.huangxq <2806701690@qq.com>
2022-12-10 17:22:58 +0800

    添加LED_DRIVE

---
+++ SDK_22_12_10/hxq/drive/LED/hxq_drv_led.c
@@ -0,0 +1,27 @@
+#include "hxq_drv_led.h"
+
+/*******************************************************************************
+* 函 数 名         : LED_Init
+* 函数功能		   : LED初始化函数
+* 输    入         : 无
+* 输    出         : 无
+*******************************************************************************/
+void LED_Init(void)
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+	
+	LED1_PORT_RCC_ENABLE;			// 使能GPIO时钟总线
+	LED2_PORT_RCC_ENABLE;			// 使能GPIO时钟总线
+	
+	GPIO_InitStructure.Pin=LED1_PIN; 
+    GPIO_InitStructure.Mode=GPIO_MODE_OUTPUT_PP;  //推挽输出
+    GPIO_InitStructure.Pull=GPIO_PULLUP;          //上拉
+    GPIO_InitStructure.Speed=GPIO_SPEED_FREQ_HIGH;//高速
+    HAL_GPIO_Init(LED1_PORT,&GPIO_InitStructure);
+	
+	GPIO_InitStructure.Pin=LED2_PIN; 
+    HAL_GPIO_Init(LED2_PORT,&GPIO_InitStructure);
+	
+	HAL_GPIO_WritePin(LED1_PORT,LED1_PIN,GPIO_PIN_SET);	   	//写入LED1低电平
+    HAL_GPIO_WritePin(LED2_PORT,LED2_PIN,GPIO_PIN_SET);		//写入LED1低电平
+} 
---
+++ SDK_22_12_10/hxq/drive/LED/hxq_drv_led.h
@@ -0,0 +1,23 @@
+#ifndef _HXQ_DRV_LEC_H
+#define _HXQ_DRV_LEC_H
+
+
+#include "system.h"
+
+
+/*  LED时钟端口、引脚定义 */
+#define LED1_PORT 				GPIOB   
+#define LED1_PIN 				GPIO_PIN_5
+#define LED1_PORT_RCC_ENABLE	__HAL_RCC_GPIOB_CLK_ENABLE()
+
+#define LED2_PORT 				GPIOE   
+#define LED2_PIN 				GPIO_PIN_5
+#define LED2_PORT_RCC_ENABLE	__HAL_RCC_GPIOE_CLK_ENABLE()
+
+
+#define LED1 PBout(5)  	
+#define LED2 PEout(5) 
+
+void LED_Init(void);
+
+#endif /* _HXQ_DRV_LEC_H */
